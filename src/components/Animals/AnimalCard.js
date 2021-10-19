a6980e856c15fdffb13cc786e166368e5c02f0b2
Kaitlyn Long <katielong11@gmail.com>
2021-10-19 12:40:43 -0500

    completed css for animal list

--- src/components/Animals/AnimalCard.js
+++ src/components/Animals/AnimalCard.js
@@ -6,15 +6,19 @@ export const AnimalCard = ({ animal, handleDeleteAnimal }) => {
     const history = useHistory();
     console.log(animal)
         return (
-        
+        <div className= "main-card">
             <div className="animal_cards">
                 <div className="header">Zoo adventures animal</div>
                 <div className="animal_card_content">
+                <picture>
+                 <img src={require(`../../images/${animal.image}`).default} alt="Dog"/>
+                </picture>
                     <h3>{animal.name}</h3>
                     <p>What I want to know: {animal.knowledge}</p>
                     <p>Located at: {animal.locationId}</p>
-                    <button type="button" onClick={() => handleDeleteAnimal(animal.id)}>Remove</button>
-                    <button type="button" onClick={() => history.push(`/animals/${animal.id}/edit`)}>Edit</button>
+                    <button className="remove_button" type="button" onClick={() => handleDeleteAnimal(animal.id)}>Remove</button>
+                    <button className="edit_button" type="button" onClick={() => history.push(`/animals/${animal.id}/edit`)}>Edit</button>
+                    </div>
                 </div>
             </div>
            
